6ae9ff4da1c771eddd403ff0faaeb1ba12f61e5d
John Rom <john@nimblelight.com>
2019-11-16 17:53:20 -0500

    Added alignment, tweaked config.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,12 +1,12 @@
 import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
 import Image from "@ckeditor/ckeditor5-image/src/image";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
 import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
 import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
 import { InsertImage } from "@johnrom/ckeditor5-oc-media/src/ckeditor5-oc-media";
@@ -26,10 +26,10 @@ OrchardCoreEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Alignment,
 	BlockQuote,
 	Heading,
 	Image,
-	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
 	InsertImage,
@@ -54,6 +54,7 @@ OrchardCoreEditor.defaultConfig = {
 			"bulletedList",
 			"numberedList",
 			"|",
+			"alignment",
 			"indent",
 			"outdent",
 			"|",
@@ -67,11 +68,13 @@ OrchardCoreEditor.defaultConfig = {
 	},
 	image: {
 		toolbar: [
-			"imageStyle:full",
-			"imageStyle:side",
+			"imageTextAlternative",
 			"|",
-			"imageTextAlternative"
-		]
+			"imageStyle:alignLeft",
+			"imageStyle:full",
+			"imageStyle:alignRight"
+		],
+		styles: ["full", "alignLeft", "alignRight"]
 	},
 	table: {
 		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 /* eslint-env node */
-
 const path = require("path");
-const webpack = require("webpack");
-const { bundler, styles } = require("@ckeditor/ckeditor5-dev-utils");
+const { styles } = require("@ckeditor/ckeditor5-dev-utils");
 const CKEditorWebpackPlugin = require("@ckeditor/ckeditor5-dev-webpack-plugin");
-const UglifyJsWebpackPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
 	devtool: "source-map",
@@ -26,30 +23,9 @@ module.exports = {
 		jquery: "jQuery"
 	},
 
-	optimization: {
-		minimizer: [
-			new UglifyJsWebpackPlugin({
-				sourceMap: true,
-				uglifyOptions: {
-					output: {
-						// Preserve CKEditor 5 license comments.
-						comments: /^!/
-					}
-				}
-			})
-		]
-	},
-
 	plugins: [
 		new CKEditorWebpackPlugin({
-			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-			language: "en",
-			additionalLanguages: "all"
-		}),
-		new webpack.BannerPlugin({
-			banner: bundler.getLicenseBanner(),
-			raw: true
+			language: "en"
 		})
 	],
 
